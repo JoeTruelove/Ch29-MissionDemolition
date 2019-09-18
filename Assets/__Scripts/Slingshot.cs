3f8929da14eb08beed639eb6f0ddbb3fe8ba652a
Joseph Truelove <43154676+JoeTruelove@users.noreply.github.com>
2019-09-18 19:45:02 -0400

    SlingShot Changes
    Added show when active, instantiated a projectile, Camera follow, and projectile trail.

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -3,42 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
-  
 
-   
-     // fields set in the Unity Inspector pane
+
+    static private Slingshot S;
+
+    // fields set in the Unity Inspector pane
     [Header("Set in Inspector")]
-
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;
 
     // fields set dynamically
     [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    private Rigidbody projectileRigidbody;
 
-
-
+    static public Vector3 Launch_POS()
+    {
+        get {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
     private void Awake()
     {
-
+        S = this;
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.setActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     private void OnMouseEnter()
     {
-
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     private void OnMouseExit()
     {
-
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 
     private void OnMouseDown()
     {
+        //The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        //Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //Start it at the launchPoint
+        projectile.tranform.position = launchPos;
+        //Set it to isKinematic for now
+        projectile.getComponent<Rigidbody>().isKinematic = true;
 
+        projectileRigidbody = projectile.getComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
     }
 
     private void Update()
     {
- 
 
+        if (!aimingMode) return true;
+
+        Vector3 mousePos2D = Input.mousePos3D - launchPos;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if(mouseDelta.magnitude>maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+
+            Vector3 projPos = launchPos + mouseDelta;
+            projectile.transform.position = projPos;
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                aimingMode = false;
+                projectileRigidbody.isKinematic = false;
+                projectileRigidbody.velocity = -mouseDelta * velocityMult;
+                FollowCam.POI = projectile;
+                projectile = null;
+            }
+        }
 
     }
 }
